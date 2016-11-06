65282603e8e67d88ad8bf21ac2616d76fa1fdf90
Grigore Stefan <g_stefan@yahoo.com>
2016-11-06 20:10:59 +0200

    update

--- check.cmd
+++ check.cmd
@@ -1,6 +1,4 @@
 @rem start dll-inject.exe --wait "X:\XYO\Source\dll-inject-sample\dll-inject-sample.dll" "notepad.exe"
 @rem start dll-inject.exe --wait "X:\XYO\Source\dll-inject-sample\dll-inject-sample.dll" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 @rem start dll-inject.exe --wait "X:\XYO\Source\dll-inject-sample\dll-inject-sample.dll" "C:\Program Files (x86)\Internet Explorer\iexplore.exe"
-@rem start dll-inject.exe --wait "X:\XYO\Source\dll-inject-sample\dll-inject-sample.dll" "X:\EasyChat.New\easychat4\activity-log\activity-log-client.exe"
 @start dll-inject.exe --wait "X:\XYO\Source\dll-inject-sample\dll-inject-sample.dll" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" https://www.google.com
-@rem start dll-inject.exe --wait "X:\XYO\Source\dll-inject-sample\dll-inject-sample.dll" "X:\EasyChat\Client\bin\easychat_controller.exe"
