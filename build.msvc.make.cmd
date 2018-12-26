900d24fa19e3c7a2ec52e83a241e2863a9b428a1
Grigore Stefan <g_stefan@yahoo.com>
2018-12-26 20:35:04 +0200

    update

--- build.msvc.make.cmd
+++ build.msvc.make.cmd
@@ -10,7 +10,7 @@ echo Error: make
 goto :eof
 :Make
 
-xyo-cc --mode=%ACTION% --dll dll-inject-sample --no-def-dynamic-link --no-lib --inc=. --use-project=libxyo-win-inject.static
+xyo-cc --mode=%ACTION% --dll-x-static dll-inject-sample --no-def-dynamic-link --no-lib --dll-no-version --inc=. --use-project=libxyo-win-inject.static
 IF ERRORLEVEL 1 goto ErrorMake
 
 
