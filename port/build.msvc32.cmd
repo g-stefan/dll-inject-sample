df9867dc9020e8ac3a7f40e053cdebf4c23a2171
Grigore Stefan <g_stefan@yahoo.com>
2020-09-24 23:23:09 +0300

    port update

--- port/build.msvc32.cmd
+++ port/build.msvc32.cmd
@@ -3,8 +3,8 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-set XYO_PATH_REPOSITORY=%HOMEDRIVE%%HOMEPATH%\SDK32\Repository
-set XYO_PATH_RELEASE=%HOMEDRIVE%%HOMEPATH%\SDK32\Release
+set XYO_PATH_REPOSITORY=%HOMEDRIVE%%HOMEPATH%\SDK32
+set XYO_PATH_RELEASE=%HOMEDRIVE%%HOMEPATH%\SDK32\release
 set XYO_PATH_REPOSITORY_LIBRARY=
 
 if "%XYO_PLATFORM%" == "win32-msvc-2019" goto Build
--- port/build.msvc64.cmd
+++ port/build.msvc64.cmd
@@ -3,8 +3,8 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-set XYO_PATH_REPOSITORY=%HOMEDRIVE%%HOMEPATH%\SDK\Repository
-set XYO_PATH_RELEASE=%HOMEDRIVE%%HOMEPATH%\SDK\Release
+set XYO_PATH_REPOSITORY=%HOMEDRIVE%%HOMEPATH%\SDK
+set XYO_PATH_RELEASE=%HOMEDRIVE%%HOMEPATH%\SDK\release
 
 if "%XYO_PLATFORM%" == "win64-msvc-2019" goto Build
 if "%XYO_PLATFORM%" == "win64-msvc-2017" goto Build
