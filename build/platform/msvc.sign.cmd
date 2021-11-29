df66b61c07e0275e50691c241c3f7eca52dd7e51
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 16:43:08 +0200

    Update

--- build/platform/msvc.sign.cmd
+++ build/platform/msvc.sign.cmd
@@ -5,13 +5,6 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 echo - %BUILD_PROJECT% ^> sign
 
-if not exist output\bin\ goto BuildStignSkipBin
-pushd output\bin
-for /r %%i in (*.dll) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
-for /r %%i in (*.exe) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
-popd
-
-:BuildStignSkipBin
 pushd output
 for /r %%i in (*.dll) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
 for /r %%i in (*.exe) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
