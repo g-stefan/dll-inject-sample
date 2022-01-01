29c0d5053a28502fd27c6343c31e1c3634911006
Grigore Stefan <g_stefan@yahoo.com>
2022-01-01 21:26:52 +0200

    Update

--- source/dll-inject-sample-version.cpp
+++ source/dll-inject-sample-version.cpp
@@ -12,10 +12,10 @@
 namespace DllInjectSample {
 	namespace Version {
 
-		static const char *version_ = "1.9.0";
-		static const char *build_ = "20";
-		static const char *versionWithBuild_ = "1.9.0.20";
-		static const char *datetime_ = "2021-11-22 17:08:07";
+		static const char *version_ = "1.11.0";
+		static const char *build_ = "21";
+		static const char *versionWithBuild_ = "1.11.0.21";
+		static const char *datetime_ = "2022-01-01 21:19:48";
 
 		const char *version() {
 			return version_;
