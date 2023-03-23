bce45f9048831b1004ea930213ab69b20b424eab
Grigore Stefan <g_stefan@yahoo.com>
2023-03-23 22:52:28 +0200

    Update

--- source/XYO/DllInjectSample/Copyright.cpp
+++ source/XYO/DllInjectSample/Copyright.cpp
@@ -14,19 +14,19 @@ namespace XYO::DllInjectSample::Copyright {
 	static const char *company_ = XYO_DLLINJECTSAMPLE_COMPANY;
 	static const char *contact_ = XYO_DLLINJECTSAMPLE_CONTACT;
 
-	std::string copyright() {
+	const char *copyright() {
 		return copyright_;
 	};
 
-	std::string publisher() {
+	const char *publisher() {
 		return publisher_;
 	};
 
-	std::string company() {
+	const char *company() {
 		return company_;
 	};
 
-	std::string contact() {
+	const char *contact() {
 		return contact_;
 	};
 
